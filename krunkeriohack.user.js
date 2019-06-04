7deee11b6e2889893c8512a24035ab7513416cbf
MrCoderN <45430918+MrCoderN@users.noreply.github.com>
2019-06-04 23:09:28 +0300

    fixed everything

--- krunkeriohack.user.js
+++ krunkeriohack.user.js
@@ -3,7 +3,7 @@
 // @description  Krunkerio Mods Features: Show FPS, Aim Fire, Auto Bunny, ESP, Adblock, Change Background
 // @namespace    iomods.org
 // @author       iomods.org
-// @version      4.0.2
+// @version      4.1
 // @require      http://code.jquery.com/jquery-3.3.1.min.js
 // @match        *://krunker.io/*
 // @grant        GM_xmlhttpRequest
@@ -41,9 +41,8 @@ GM_xmlhttpRequest({
                 html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/bypass.js"></script>';
                 html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/haxy.js"></script>';
                 html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/game.js"></script>';
-     const bot = new Krunkerio_net();
+                     const bot = new Krunkerio_net();
     bot.init();
-                document.open();
                 document.write(html);
                 document.close();
             }
@@ -51,40 +50,13 @@ GM_xmlhttpRequest({
     }
 })
 
-var checkgameloaded;
-window.WebSocket.prototype.oldSend = WebSocket.prototype.send;
-window.WebSocket.prototype.send = function(m){
-    if (!checkgameloaded){
-        activmod(this);
-    }
-    this.oldSend(m);
+function setaps(eID){
+        document.getElementById(eID).insertAdjacentHTML('afterbegin', '<a target="_blank" style="font-weight:bold;color:yellow;text-decoration:none;" href="https://bit.ly/2E0JJHx">KRUNKRIO.NET</a> - <a target="_blank" style="font-weight:bold;color:yellow;text-decoration:none;" href="https://bit.ly/2Okhczb">KRUNKRIO.ORG</a>  - <a target="_blank" style="font-weight:bold;color:red;text-decoration:none;" href="https://bit.ly/2Z3XxaI">IOMDS.ORG</a>  - <a target="_blank" style="font-weight:bold;color:green;text-decoration:none;" href="https://bit.ly/2EgRmIx">SLITHRE.COM</a> - <a target="_blank" style="font-weight:bold;color:cyan;text-decoration:none;" href="https://bit.ly/30tvJxW">SKRIBBLIO.NET</a>  - <a target="_blank" style="font-weight:bold;color:yellow;text-decoration:none;" href="https://shellshockio.org">SHELLSHOCKIO.ORG</a>  - <a target="_blank" style="font-weight:bold;color:yellow;text-decoration:none;" href="https://moomooioplay.com">MOOMOOIOPLAY.COM</a></br><span style="color:white">Fix of script detected:</span> <a href="https://www.youtube.com/watch?v=HjwhMNDTlZA" target="_blank">watch this video (click)</a>');
 }
-
-window.chatmessage = window.Ze = (t, e, i) => {
-    for (chatList.innerHTML += i ? "<div class='chatItem'><span class='chatMsg'>" + e + "</span></div><br/>" : "<div class='chatItem'>" + (t || "unknown") + ": <span class='chatMsg'>" + e + "</span></div><br/>"; 250 <= chatList.scrollHeight;) chatList.removeChild(chatList.childNodes[0])
-}
-
-//default keys
-var keys;
-(function (keys) {
-    keys["one"] = "T";
-    keys["two"] = "Y";
-    keys["three"] = "U";
-    keys["four"] = "I";
-    keys["five"] = "O";
-    keys["six"] = "P";
-    keys["seven"] = "B";
-})(keys || (keys = {}));
-
-var speeder;(function (speeder) {speeder["on"] = "1.250";speeder["info"] = "Speed Hack (?)";})(speeder || (speeder = {}));
-
-function activmod(socket){
-    window.socket = socket;
-    checkgameloaded = socket;
-
-    window.chatmessage("Krunkerio.org", `Welcome to <span style="color: red;">Krunkerio.net</span> <span style="color: yellow;">Check below the Leaderboard</span> in game for shortcuts`);
-    $("#subLogoButtons").html('<div class="button small" onmouseenter="playTick()" onclick="openHostWindow();window.open(\'https://goo.gl/FGU9pC\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">Host Game</div><div id="inviteButton" class="button small" onmouseenter="playTick()" onclick="copyInviteLink();window.open(\'https://goo.gl/XCNoJL\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">Invite</div><div class="button small" onmouseenter="playTick()" onclick="showWindow(2)">Server Browser</div><div class="button small" onmouseenter="playTick()" onclick="window.open(\'https://goo.gl/6kqrgN\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">KRUNKER.IO HACKS</div><div class="button small" onmouseenter="playTick()" onclick="window.open(\'https://goo.gl/XCNoJL/\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">OTHER .IO CHEATS</div>');
-    $("#healthHolder").append('<a style=\"color:yellow;top:1520px;\" href="https://goo.gl/XCNoJL" target="_blank">SLITHERE.COM</a>');
+var yourclose = setInterval(function() {
+    var setA=document.getElementsByClassName("ad-block-leaderboard-bottom")[0];
+    setaps("aHolder");
+    $("#subLogoButtons").html('<div class="button small" onmouseenter="playTick()" onclick="openHostWindow();window.open(\'https://goo.gl/FGU9pC\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">Host Game</div><div id="inviteButton" class="button small" onmouseenter="playTick()" onclick="copyInviteLink();window.open(\'https://goo.gl/XCNoJL\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">Invite</div><div class="button small" onmouseenter="playTick()" onclick="showWindow(2)">Server Browser</div><div class="button small" onmouseenter="playTick()" onclick="window.open(\'https://goo.gl/6kqrgN\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">KRUNKR.IO HACKS</div><div class="button small" onmouseenter="playTick()" onclick="window.open(\'https://goo.gl/XCNoJL/\', \'_blank\', \'location=yes,height=570,width=520,scrollbars=yes,status=yes\');">OTHER .IO MODS</div>');
 
     //values of scripts
     var values = ["location=yes,scrollbars=yes,status=yes,height=570,width=520","location=yes,scrollbars=yes,status=yes,left=2000,height=570,width=520"],
@@ -104,29 +76,16 @@ function activmod(socket){
         links["one"] = links[0];
         links["nine"] = links[9];
     })(links || (links = {}));
-    //tanitim belgeseli
-    var colorize,lnk,text,ministyler
-    lnk = ["SLITHERE.COM", "KRUNKERIO.ORG", "KRUNKERIO.NET", "SHELLSHOCKIO.ORG", "MOOMOOIOPLAY.COM", "SURVIVIO.INFO", "ZOMBSROYALEIO.ORG", "MOPE-IO.NET", "MOPEIOGAME.COM", "DIEPIOPLAY.COM", "DIEPIOPLAY.ORG", "SLITHERIOPLAY.ORG", "SKRIBBL-IO.NET", "SPINZ-IO.NET", "BONK-IO.NET", "DEEEEP-IO.NET", "IOGAMESLIST.ORG", "IOMODS.ORG"];
-    text = "<b>";
-    lnk.forEach(lnkfunc);
-    text += "</b>";
 
-    function lnkfunc(value) {
-        var value2 = value;
-        if(value == "SLITHERE.COM" || value == "KRUNKERIO.ORG" || value == "IOMODS.ORG" || value == "KRUNKERIO.NET") { colorize = true; } else { colorize = false; }
-        if(value == "MOPE-IO.NET") { value2="MOPEIO.NET"; } if(value == "BONK-IO.NET") { value2="BONKIO.NET"; } if(value == "SPINZ-IO.NET") { value2="SPINZIO.NET"; } if(value == "DEEEEP-IO.NET") { value2="DEEEEPIO.NET"; } if(value == "SKRIBBL-IO.NET") { value2="SKRIBBLIO.NET"; } if(value == "IO-OYUNLAR.COM") { value2="IOOYUNLAR.COM"; }
-        if(colorize == false){ministyler = "color:white;font-size:11px;padding:0px;";} else {ministyler = "color:yellow;font-size:11px;padding:0px;";}
-        text += '<a href="http://'+value+'" target="_blank" style="'+ministyler+'">'+value2+'</a> - ';
-    }
     //genel isimlendirme ve ayarlar
     this.settings = {
-        feature1: "Show FPS",
-        feature2: "Game Menu",
-        feature3: "Auto Aim",
-        feature4: "Show ESP",
-        feature5: "Change Background",
+        feature1: "Show ESP",
+        feature2: "Show Game Menu",
+        feature3: "Anti Script Detected",
+        feature4: "Anti Disconnect",
+        feature5: "Background Color",
         feature6: "Rainbow Background",
-        feature7: "Zoom IN/OUT",
+        feature7: "Zoom Hack",
         string: "<a style=\"padding-right: 12px;\"></a> <font color=\"black\">-</font> <a style=\"padding-left: 12px;\"></a>",
         buttonpadder: "padding-left: 2px;height:22px;",
         box: "padding-right:40px;box-sizing: border-box;width: 50%;border: 10px solid black;float: left;",
@@ -134,7 +93,7 @@ function activmod(socket){
         optionstyler3: "color:black;font-size:10px;",
         keycolor: "color:#333333;",
         keystyle: "font-size:10px;",
-        hayirdir: "<span>This</span> <span style=\"color:red\">script</span> <span style=\"color:blue\">stolen</span> <span style=\"color:green\">from</span> <a href=\"https://goo.gl/6kqrgN\" target=\"_blank\"><b>Krunkerio.net</b></a>",
+        hayirdir: "<span>This</span> <span style=\"color:red\">script</span> <span style=\"color:blue\">stolen</span> <span style=\"color:green\">from</span> <a href=\"https://goo.gl/6kqrgN\" target=\"_blank\"><b>Krunkrio.net</b></a>",
         formstyle: "border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(245, 245, 245, 1.0);",
         fpsstyle: "border:1px solid black;border-radius:20px;padding:3px;width:85px;height:25px;font-size: 15px;text-align:center;background-color: rgba(0, 0, 0, 0.8);color:white;",
         tablostyle: "border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(255, 255, 255, 0.3);",
@@ -166,7 +125,6 @@ function activmod(socket){
     $("#signedOutHeaderBar").append('<div style="'+this.settings.fpsstyle+'" id="fps" class="fps"></div>');
     $("#signedInHeaderBar").append('<span style="margin-right:6px;"></span><div style="'+this.settings.fpsstyle+'" id="fps2" class="fps2"></div>');
     $('#topLeftHolder').append('<div style="'+this.settings.fpsstyle+'" id="fps3" class="fps3"></div>');
-    $('#aHolder').prepend('<div class="list1"></div>');
     //general
     $('.option1').html('<a style="'+this.settings.optionstyler+'" href="http://'+linkToUse+'" target="blank">'+this.settings.feature1+'</a> <a><label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' class="fps" onchange="window.open(\'http://'+linkToUse+'\', \'_blank\', \''+valueToUse2+'\');" checked><span class=\'slider\'></span></label></a><div class="option2"></div>');
     $('.option1').on('click', '.fps', function() { hideandseek(); });
@@ -178,16 +136,11 @@ function activmod(socket){
     $('.option5').on('change', '.bgcont', function() { changebackground(); });
     $('.option6').html('<a style="'+this.settings.optionstyler+'" href="http://'+linkToUse5+'" target="blank">'+this.settings.feature6+'</a> <a><label style="'+this.settings.buttonpadder+'" class=\'switch\'><input type=\'checkbox\' class="renkcont" onchange="window.open(\'http://'+linkToUse5+'\', \'_blank\', \''+valueToUse2+'\');"><span class=\'slider\'></span></label></a><div class="option7"></div>');
     $('.option6').on('change', '.renkcont', function() { colorfulmod(); });
-    $('.option7').html('<a style="'+this.settings.optionstyler+'" href="http://'+linkToUse6+'" target="blank">'+this.settings.feature7+'</a> <input name="zoom" id="zoom" type="number" style="width: 2em;font-size:10px;" min="70" max="140" step="1" value="100" class="zoom" oninput="amount.value=zoom.value;" onchange="window.open(\'http://'+linkToUse6+'\', \'_blank\', \''+valueToUse3+'\');"> <output style="'+this.settings.optionstyler+'" id="amount" name="amount" for="zoom">"100"</output> <a style="'+this.settings.optionstyler3+'" href="http://'+linkToUse4+'" target="blank">(Min: 70-Max: 140)</a><div class="keylist"></div>');
+       $('.option7').html('<a style="'+this.settings.optionstyler+'" href="http://'+linkToUse6+'" target="blank">'+this.settings.feature7+'</a> <input name="zoom" id="zoom" type="number" style="width: 2em;font-size:10px;" min="70" max="140" step="1" value="100" class="zoom" oninput="amount.value=zoom.value;" onchange="window.open(\'http://'+linkToUse6+'\', \'_blank\', \''+valueToUse3+'\');"> <output style="'+this.settings.optionstyler+'" id="amount" name="amount" for="zoom">"100"</output> <a style="'+this.settings.optionstyler3+'" href="http://'+linkToUse4+'" target="blank">(Min: 70-Max: 140)</a>');
     $('.option7').on('input', '.zoom', function(e) { zoominout(); });
     //main keys
-    $('.keylist').html('<span style="'+this.settings.keystyle+'"><a style="'+this.settings.keycolor+'" href="http://'+linkToUse+'" target="blank">Aimbot:</a> <input maxlength="1" type=\'text\' value="'+keys.one+'" style="width: 2em;font-size:10px;" class="key1" onclick="window.open(\'http://'+linkToUse+'\', \'_blank\', \''+valueToUse+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse2+'" target="blank">Auto Aim Range:</a> <input maxlength="1" type=\'text\' value="'+keys.two+'" style="width: 2em;font-size:10px;" class="key2" onclick="window.open(\'http://'+linkToUse2+'\', \'_blank\', \''+valueToUse2+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse3+'" target="blank">ESP:</a> <input maxlength="1" type=\'text\' value="'+keys.three+'" style="width: 2em;font-size:10px;" class="key3" onclick="window.open(\'http://'+linkToUse3+'\', \'_blank\', \''+valueToUse3+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse4+'" target="blank">Unlimited Ammo:</a> <input maxlength="1" type=\'text\' value="'+keys.four+'" style="width: 2em;font-size:10px;" class="key4" onclick="window.open(\'http://'+linkToUse4+'\', \'_blank\', \''+valueToUse+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse5+'" target="blank">No Recoil:</a> <input maxlength="1" type=\'text\' value="'+keys.five+'" style="width: 2em;font-size:10px;" class="key5" onclick="window.open(\'http://'+linkToUse5+'\', \'_blank\', \''+valueToUse2+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse6+'" target="blank">Auto Reload:</a> <input maxlength="1" type=\'text\' value="'+keys.six+'" style="width: 2em;font-size:10px;" class="key6" onclick="window.open(\'http://'+linkToUse6+'\', \'_blank\', \''+valueToUse3+'\');" readonly> - <a style="'+this.settings.keycolor+'" href="http://'+linkToUse+'" target="blank">Bunny Hop:</a> <input maxlength="1" type=\'text\' value="'+keys.seven+'" style="width: 2em;font-size:10px;" class="key7" onclick="window.open(\'http://'+linkToUse+'\', \'_blank\', \''+valueToUse+'\');" readonly></br><span style="color:red;">How To Fix Script Detected?</span> --> <a href="https://www.youtube.com/watch?v=HjwhMNDTlZA" target="_blank">VIDEO ABOUT IT (click) - - https://www.youtube.com/watch?v=HjwhMNDTlZA</a>');
-    //change of keys
-    $('.keylist').on('change', function() { keyschange(); });
-    $('.keylist').on('change', '.sp1', function() { speedchange(); });
-    //elementlist
-    $('.list1').html('<div style="'+this.settings.liststyler+'">'+text+'</div>');
-    //fps counter
+
+     //fps counter
     var before,now,fps
     before=Date.now();
     fps=0;
@@ -197,7 +150,6 @@ function activmod(socket){
             fps=Math.round(1000/(now-before));
             before=now;
             requestAnimationFrame(loop);
-            if(this.settings.feature5!="Change Background") {logger.crash('error please re-download it from <b><a href="https://iomods.org" target="_blank">www.ioMods.org</a></b>');}
             document.getElementById('fps').innerHTML = 'FPS: ' + fps;
             document.getElementById('fps2').innerHTML = 'FPS: ' + fps;
             document.getElementById('fps3').innerHTML = 'FPS: ' + fps;
@@ -280,14 +232,10 @@ function activmod(socket){
             $('body').css('zoom',''+findinput+'%');
         } else { $('body').css('zoom','100%'); }
     }
-}
+    clearInterval(yourclose);
+},1000);
 
 
-var OnOffMode;
-(function (OnOffMode) {
-    OnOffMode["On"] = "<span style=\"color:green;\">ON</span>";
-    OnOffMode["Off"] = "<span style=\"color:gray;\">OFF</span>";
-})(OnOffMode || (OnOffMode = {}));
 class Module {
     constructor() {
         this.allStates = this.getAllModes();
@@ -320,124 +268,12 @@ class Module {
     }
 }
 
-class aimbot extends Module {
-    getName() {
-        return 'Aimbot';
-    }
-    getKey() {
-        return ''+keys.one+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class aimrange extends Module {
-    getName() {
-        return 'Aim Range';
-    }
-    getKey() {
-        return ''+keys.two+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class esp extends Module {
-    getName() {
-        return 'ESP';
-    }
-    getKey() {
-        return ''+keys.three+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class unammo extends Module {
-    getName() {
-        return 'Unlimited Ammo';
-    }
-    getKey() {
-        return ''+keys.four+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class norec extends Module {
-    getName() {
-        return 'No Recoil';
-    }
-    getKey() {
-        return ''+keys.five+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class autoreload extends Module {
-    getName() {
-        return 'Auto Reload';
-    }
-    getKey() {
-        return ''+keys.six+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
-class bunny extends Module {
-    getName() {
-        return 'Bunny Hop';
-    }
-    getKey() {
-        return ''+keys.seven+'';
-    }
-    getAllModes() {
-        return [OnOffMode.Off, OnOffMode.On];
-    }
-    getInitialMode() {
-        return OnOffMode.On;
-    }
-}
-
 class Krunkerio_net {
     constructor() {
         this.modules = [];
         this.values='Krunkerio.org<hr>Krunkerio.net';
     }
     init() {
-        this.modules.push(new aimbot());
-        this.modules.push(new aimrange());
-        this.modules.push(new esp());
-        this.modules.push(new unammo());
-        this.modules.push(new norec());
-        this.modules.push(new autoreload());
-        this.modules.push(new bunny());
         const initInfoBoxInterval = setInterval(() => {
             if (this.canInjectInfoBox()) {
                 clearInterval(initInfoBoxInterval);
@@ -451,17 +287,12 @@ class Krunkerio_net {
         if (infoBox === null) {
             return;
         }
-        const moduleLines = this.modules.map(module => {
-            return `
-<div class="leaderItem">
-<div class="leaderNameF">[${module.getKey().toUpperCase()}] ${module.getName()}</div>
-<div class="leaderScore"></div>
-</div>
-`;
-        });
         infoBox.innerHTML = `
 <div class="krunkbotTitle">${this.values}</div>
-${moduleLines.join('')}
+<div class="leaderItem">
+<div class="leaderNameF">[T] Aimbot</br>[B] BunnyHop</br>[Y] Aim Range</br>[U] ESP</div>
+<div class="leaderScore"></div>
+</div>
 `.trim();
     }
     injectInfoBox() {
